026511d170926dcf5779a827eddf9618893e01de
Synchroneyes <dev@monvoisin-kevin.fr>
2025-05-14 20:55:24 +0200

    fix: url change

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,28 +3,28 @@ export module api {
     export function getMapsList() {
         return axios({
             method: 'get',
-            url: "https://api.mc.monvoisin-kevin.fr" + '/maps.json'
+            url: "https://api.mineralcontest.fr" + '/maps.json'
         })
     }
 
     export function getFilesList() {
         return axios({
             method: 'get',
-            url: "https://api.mc.monvoisin-kevin.fr" + '/files.json'
+            url: "https://api.mineralcontest.fr" + '/files.json'
         })
     }
 
     export function getBlogList() {
         return axios({
             method: 'get',
-            url: "https://api.mc.monvoisin-kevin.fr" + '/blog.json'
+            url: "https://api.mineralcontest.fr" + '/blog.json'
         })
     }
 
     export function getArticleContent(title: string) {
         return axios({
             method: 'get',
-            url: "https://api.mc.monvoisin-kevin.fr" + '/blog/' + title
+            url: "https://api.mineralcontest.fr" + '/blog/' + title
         })
     }
 
